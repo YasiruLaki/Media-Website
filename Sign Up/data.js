d98f83d8fd33fbfaea18273856c27c1bd7ea9001
Yasiru Lakintha <yasiru.upwork@gmail.com>
2022-01-27 22:54:01 +0530

    .

--- Sign Up/data.js
+++ Sign Up/data.js
@@ -1,4 +1,5 @@
-const firebaseConfig = {
+// Firebase configuration
+  var firebaseConfig = {
     apiKey: "AIzaSyB_-s9xtEs-kfcGK2AjZsbu_V4FCEJ6EaY",
     authDomain: "signuppage-1cd22.firebaseapp.com",
     projectId: "signuppage-1cd22",
@@ -7,46 +8,62 @@ const firebaseConfig = {
     appId: "1:145475644240:web:e02980cbf370b33e4b1a55",
     measurementId: "G-XKZ6T031YG"
   };
-  
   // Initialize Firebase
   firebase.initializeApp(firebaseConfig);
   firebase.analytics();
 
+  const auth =  firebase.auth();
+  const datab  = firebase.database().ref('data');
 
-  //code part//
+  //signup function
+  function signUp(){
+    var email = document.getElementById("email");
+    var password = document.getElementById("password");
 
-  var datab  = firebase.database().ref('data');
-function UserRegister(){
-var email = document.getElementById('eemail').value;
-var password = document.getElementById('lpassword').value;
-firebase.auth().createUserWithEmailAndPassword(email,password).then(function(){
-    
-}).catch(function (error){
-    var errorcode = error.code;
-    var errormsg = error.message;
-});
-}
-const auth = firebase.auth();
-function SignIn(){
-    var email = document.getElementById('eemail').value;
-    var password = document.getElementById('lpassword').value;
-    const promise = auth.signInWithEmailAndPassword(email,password);
-    promise.catch( e => alert(e.msg));
-
-}
 
 document.getElementById('form').addEventListener('submit', (e) => {
     e.preventDefault();
     var userInfo = datab.push();
     userInfo.set({
-        name: getId('fname'),
-        email : getId('eemail'),
-        password : getId('lpassword')
-    });
-    alert("Successfully Signed Up");
-    console.log("sent");
-    document.getElementById('form').reset();
+        name: getId('username'),
+        email : getId('email'),
+        password : getId('password')
+    })
 });
-function  getId(id){
-    return document.getElementById(id).value;
+
+const promise = auth.createUserWithEmailAndPassword(email.value,password.value);
+    //
+    promise.catch(e=>alert(e.message));
+    alert("SignUp Successfully!");
+
+    document.getElementById('form').reset();
 }
+
+
+  //signIN function
+  function  signIn(){
+    var email = document.getElementById("email");
+    var password  = document.getElementById("password");
+    const promise = auth.signInWithEmailAndPassword(email.value,password.value);
+    promise.catch(e=>alert(e.message));
+    
+  }
+
+
+  //signOut
+
+  function signOut(){
+    auth.signOut();
+    alert("SignOut Successfully from System");
+  }
+
+  //active user to Alert
+  firebase.auth().onAuthStateChanged((user)=>{
+    if(user){
+      var email = user.email;
+      alert("Active user "+email);
+
+    }else{
+      alert("No Active user Found")
+    }
+  })
