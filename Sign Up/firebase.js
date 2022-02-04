5b8d113f854fbc43343eba6f4a44d0a1d33875af
Yasiru Lakintha <yasiru.upwork@gmail.com>
2022-02-04 16:03:48 +0530

    Sign in page modified

---
+++ Sign Up/firebase.js
@@ -0,0 +1,14 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyB_-s9xtEs-kfcGK2AjZsbu_V4FCEJ6EaY",
+    authDomain: "signuppage-1cd22.firebaseapp.com",
+    databaseURL: "https://signuppage-1cd22-default-rtdb.firebaseio.com",
+    projectId: "signuppage-1cd22",
+    storageBucket: "signuppage-1cd22.appspot.com",
+    messagingSenderId: "145475644240",
+    appId: "1:145475644240:web:e02980cbf370b33e4b1a55",
+    measurementId: "G-XKZ6T031YG"
+  };
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
