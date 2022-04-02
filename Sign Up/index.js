c6a44b4240a5c031062c63844aa12710ca88486a
Yasiru Lakintha <yasiru.upwork@gmail.com>
2022-04-02 15:56:51 +0530

    Sign Up page updated and added a latest update

--- Sign Up/index.js
+++ Sign Up/index.js
@@ -1,46 +1,51 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyB_-s9xtEs-kfcGK2AjZsbu_V4FCEJ6EaY",
+    authDomain: "signuppage-1cd22.firebaseapp.com",
+    databaseURL: "https://signuppage-1cd22-default-rtdb.firebaseio.com",
+    projectId: "signuppage-1cd22",
+    storageBucket: "signuppage-1cd22.appspot.com",
+    messagingSenderId: "145475644240",
+    appId: "1:145475644240:web:e02980cbf370b33e4b1a55",
+    measurementId: "G-XKZ6T031YG"
+  };
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.auth = getAuth(app);
+  
+
+
+
+//Login & Register Switching
+
 document.getElementById("loginForm").addEventListener("submit",(event)=>{
     event.preventDefault()
 })
 
-  //active user to Alert
-  firebase.auth().onAuthStateChanged((user)=>{
-    if(user){
-      var email = user.email;
-      location.replace("welcome.html")
-      alert("Active user " +email);
-      
+document.getElementById("login").addEventListener('click', function(){
+  document.getElementById("reg").style.display="inline";
+  document.getElementById("login").style.display="none";
+});
 
-    }else{
-      alert("No Active user Found")
-    }
-  })
+document.getElementById("reg").addEventListener('click', function(){
+  document.getElementById("login").style.display="inline";
+  document.getElementById("reg").style.display="none";
+});
 
+
+//Login
 
 function login(){
-    const email = document.getElementById("email").value
-    const password = document.getElementById("password").value
+    const email = document.getElementById("login-email").value
+    const password = document.getElementById("login-password").value
     firebase.auth().signInWithEmailAndPassword(email, password)
+    .then((userCredential) => {
+        const user = userCredential.user;
+        location.replace("welcome.html")
+        document.getElementById("user").innerHTML = "Hello, "+user.email
+    })
+
     .catch((error)=>{
         document.getElementById("error").innerHTML = error.message
     })
-}
-
-function signUp(){
-    const email = document.getElementById("email").value
-    const password = document.getElementById("password").value
-    firebase.auth().createUserWithEmailAndPassword(email, password)
-    .catch((error) => {
-        document.getElementById("error").innerHTML = error.message
-    });
-}
-
-function forgotPass(){
-    const email = document.getElementById("email").value
-    firebase.auth().sendPasswordResetEmail(email)
-    .then(() => {
-        alert("Reset link sent to your email id")
-    })
-    .catch((error) => {
-        document.getElementById("error").innerHTML = error.message
-    });
-}
+} 
--- Sign Up/welcome.js
+++ Sign Up/welcome.js
@@ -1,10 +1,3 @@
-firebase.auth().onAuthStateChanged((user)=>{
-    if(!user){
-        location.replace("index.html")
-    }else{
-        document.getElementById("user").innerHTML = "Hello, "+user.email
-    }
-})
 
 
 function logout(){
