52e3bc9da11c00ed9b196ba6bbb22a4b01d9d153
Yasiru Lakintha <yasiru.upwork@gmail.com>
2022-07-21 00:53:21 +0530

    .

--- Sign Up/app.js
+++ Sign Up/app.js
@@ -1,6 +1,6 @@
-// Your web app's Firebase configuration
-  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-  var firebaseConfig = {
+
+function initializeApp() {
+const firebaseConfig = {
     apiKey: "AIzaSyB_-s9xtEs-kfcGK2AjZsbu_V4FCEJ6EaY",
     authDomain: "signuppage-1cd22.firebaseapp.com",
     databaseURL: "https://signuppage-1cd22-default-rtdb.firebaseio.com",
@@ -9,60 +9,5 @@
     messagingSenderId: "145475644240",
     appId: "1:145475644240:web:e02980cbf370b33e4b1a55",
     measurementId: "G-XKZ6T031YG"
-  };
-  // Initialize Firebase
-  firebase.initializeApp(firebaseConfig);
-
-  const auth =  firebase.auth();
-
-  //signup function
-  function signUp(){
-    var username = document.getElementById("username-reg");
-    var email = document.getElementById("email-reg");
-    var password = document.getElementById("pass-reg");
-
-    const promise = auth.createUserWithEmailAndPassword(email.value,password.value);
-    //
-    promise.catch(e=>alert(e.message));
-    alert("SignUp Successfully");
-
-    promise.catch((error) => {
-       document.getElementById("error-code").innerHTML = error.message
-      // ..
-    });
-  }
-
-  //signIN function
-  function  signIn(){
-    var email = document.getElementById("email-login");
-    var password  = document.getElementById("pass-login");
-    const promise = auth.signInWithEmailAndPassword(email.value,password.value);
-    promise.catch(e=>alert(e.message));
-
-    promise.catch((error) => {
-      document.getElementById("error-code").innerHTML = error.message
-     // ..
-   });
-    
-  }
-
-  //ckeck
-
-
-  //signOut
-
-  function signOut(){
-    auth.signOut();
-    alert("SignOut Successfully from System");
-  }
-
-  //active user to homepage
-  firebase.auth().onAuthStateChanged((user)=>{
-    if(user){
-      var email = user.email;
-      alert("Active user "+email);
-
-    }else{
-      alert("No Active user Found")
-    }
-  })
+};
+}
